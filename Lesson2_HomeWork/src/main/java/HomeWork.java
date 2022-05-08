ed088244f488a91059a6984a2cb6b90703714371
anton <antonlazovski111@gmail.com>
2022-05-08 23:37:15 +0300

    Lesson 2. Домашнее задание(типы данных)

---
+++ Lesson2_HomeWork/src/main/java/HomeWork.java
@@ -0,0 +1,24 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        printLiterals();
+
+    }
+    private static void printLiterals() {
+        boolean a = true;
+        String b = "Home" + " " + "Work";
+        System.out.println(b);
+        System.out.println('$');
+        int c = 0b010111010;
+        System.out.println(c);
+        int d = 0144562;
+        System.out.println(d);
+        int e = 1984;
+        System.out.println(e);
+        int f = 0x1586A4D4;
+        System.out.println(f);
+        float g = 4.05E-13f;
+        System.out.println(g);
+        double h = 3.14;
+        System.out.println(h);
+    }
+}
